3c38d61b27f5df2becd88dc03c08a976d50725b2
queenprotea <yizuzelabautista@gmail.com>
2026-09-22 18:37:40 -0600

    wip: rooms page

--- src/Torres.Client/Screens/RoomsScreen.cs
+++ src/Torres.Client/Screens/RoomsScreen.cs
@@ -1,3 +1,4 @@
+using Myra.Events;
 using Myra.Graphics2D.UI;
 
 using Torres.Client.Localization;
@@ -15,18 +16,106 @@ namespace Torres.Client.Screens
 
         protected override Widget Build()
         {
-            VerticalStackPanel panel =  new VerticalStackPanel();
+            VerticalStackPanel panel =  Page();
+            panel.Spacing = Theme.FieldSpacing;
             panel.Widgets.Add(BuildSearchRoom());
+            panel.Widgets.Add(BuildAvaliableRoomsHeader());
             
             return panel;
         }
 
         private Widget BuildSearchRoom()
         {
-            HorizontalStackPanel row = new HorizontalStackPanel();
-            row.Widgets.Add(new LabeledTextBox( TextKeys.Rooms.SearchPlaceholder, false) );
-
+            HorizontalStackPanel row = Row();
+            
+            var searchBox = new LabeledTextBox(TextKeys.Rooms.SearchPlaceholder, false);
+            searchBox.VerticalAlignment = VerticalAlignment.Center;
+            
+            LocalizedButton searchButton = SecondaryButton(TextKeys.Rooms.SearchButton);
+            searchButton.Click += OnSearchButtonClicked;
+            searchButton.VerticalAlignment = VerticalAlignment.Bottom;
+            
+            LocalizedButton createRoomButton = PrimaryButton(TextKeys.Rooms.CreateRoomButton);
+            createRoomButton.Click += OnCreateRoomButtonClicked;
+            createRoomButton.VerticalAlignment = VerticalAlignment.Bottom;
+            
+            row.Widgets.Add(searchBox);
+            row.Widgets.Add(searchButton);
+            row.Widgets.Add(createRoomButton);
+            
+            StackPanel.SetProportionType(searchBox, ProportionType.Fill);
+            
             return row;
+        }
+
+        private HorizontalStackPanel BuildAvaliableRoomsHeader()
+        {
+            var row = new HorizontalStackPanel();
+            
+            LocalizedButton refreshButton = SecondaryButton(TextKeys.Rooms.RefreshButton);
+            refreshButton.Click += OnRefreshButtonClicked;
+            refreshButton.VerticalAlignment = VerticalAlignment.Bottom;
+            refreshButton.HorizontalAlignment = HorizontalAlignment.Right;
+
+            var onlyPublic = Title(TextKeys.Rooms.PublicOnlyBadge);
+            onlyPublic.VerticalAlignment = VerticalAlignment.Bottom;
+
+            var avaliableRooms = Title(TextKeys.Rooms.AvailableRoomsTitle);
+            avaliableRooms.VerticalAlignment = VerticalAlignment.Bottom;
+            
+            row.Widgets.Add(avaliableRooms);
+            row.Widgets.Add(onlyPublic);
+            row.Widgets.Add(refreshButton);
+       
+            StackPanel.SetProportionType(onlyPublic, ProportionType.Fill);
+            
+            return row;
+        }
+
+        private VerticalStackPanel BuildAvaliableRoomsFooter()
+        {
+            var row = new VerticalStackPanel();
+            
+            
+            
+        }
+        private void OnSearchButtonClicked(object sender, MyraEventArgs arguments)
+        {
+            //TODO: Implement search functionality
+        }
+        
+        private void OnCreateRoomButtonClicked(object sender, MyraEventArgs arguments)
+        {
+            //TODO: Implement create room functionality
+        }
+
+        private void OnRefreshButtonClicked(object sender, MyraEventArgs arguments)
+        {
+            //TODO: Implement refresh avaliable rooms functionality
+        }
+
+        private VerticalStackPanel FillRoomsList(Room[] rooms)
+        {
+            var avalaibleRooms = new VerticalStackPanel();
+            
+            if (rooms.Length == 0)
+            {
+                return avalaibleRooms;
+            }
+            
+            foreach (var Room in Rooms)
+            {
+                var row = new HorizontalStackPanel();
+                row = buildRoomRow(Room);
+                avalaibleRooms.Widgets.Add(row);
+            }
+
+            return avalaibleRooms;
+        }
+
+        private HorizontalStackPanel buildRoomRow(Room Room)
+        {
+            var row = new 
         }
     }
 }
